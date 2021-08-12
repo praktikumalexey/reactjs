168409458e0b913f334ca300d8493f6bf9a73ec0
alexey <praktikum.alexey@yandex.ru>
2021-08-12 21:35:29 +0300

    init

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "*.png" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
