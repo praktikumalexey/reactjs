fff585dbb7583edabcbeb157bbf40fb5eea527a3
alexey <praktikum.alexey@yandex.ru>
2021-08-13 10:24:12 +0300

    add css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import * as data from "./utils/data.json";
+import syles from "./styles/index.module.css";
+// import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+
+console.log(data[0], syles);
 
 function App(): React.ReactElement {
   return (
     <div className="App">
+      {/* <Logo /> */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p className={syles.redText}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
--- src/types.d.ts
+++ src/types.d.ts
@@ -4,6 +4,11 @@ declare module "*.png" {
   export default content;
 }
 
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
+
 declare module "*.svg" {
   const content: any;
   export default content;
